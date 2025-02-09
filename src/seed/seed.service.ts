90af312f86ad81df8ca7c06d01dbb866d453a727
Manuel Ulate <fr3zs09@gmail.com>
2025-02-09 11:14:38 -0600

    feat(seed):pokemon module imported

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { type AxiosInstance } from 'axios';
 
 import { type PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios:AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel:Model<Pokemon>
+  ) {}
+
   async execute() {
     const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
 
-    data.results.forEach(({ name, url }) => {
+
+    data.results.forEach( async ({ name, url }) => {
 
       const segments = url.split('/');
-      const no:number = +segments[ segments.length - 2 ];
+      const no:number = +segments[ segments.length - 2 ]; 
+
+      // Inefficient way to create a document
+      await this.pokemonModel.create({ name, no });
 
     });
 
