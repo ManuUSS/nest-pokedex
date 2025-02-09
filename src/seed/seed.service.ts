b98523bf38d6402cc3b3848c12b07744bcbd7f18
Manuel Ulate <fr3zs09@gmail.com>
2025-02-09 11:19:43 -0600

    feat(seed):pokemons inserted on db

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -17,18 +17,35 @@ export class SeedService {
   ) {}
 
   async execute() {
+
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
 
+    // const insertPromisesArray: Promise<Pokemon>[] = [];
 
-    data.results.forEach( async ({ name, url }) => {
+    const pokemonsToInsert: { name:string, no:number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
 
       const segments = url.split('/');
       const no:number = +segments[ segments.length - 2 ]; 
 
       // Inefficient way to create a document
-      await this.pokemonModel.create({ name, no });
+      // await this.pokemonModel.create({ name, no });
+
+      // More efficient way to create a document
+      // insertPromisesArray.push( 
+      //   this.pokemonModel.create({ name, no })
+      // );
+
+      // Optimal way to create a document
+      pokemonsToInsert.push({ name, no });
 
     });
+
+    // await Promise.all( insertPromisesArray );
+    await this.pokemonModel.insertMany( pokemonsToInsert );
 
     return data.results;
   }
