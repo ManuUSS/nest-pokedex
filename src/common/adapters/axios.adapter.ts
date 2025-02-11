539d57f2508fb7293fc9bf9bbb69f3ff66a941c5
Manuel Ulate <fr3zs09@gmail.com>
2025-02-10 20:07:24 -0600

    feat(seed):axios adapter pattern implemented on seed module

--- src/common/adapters/axios.adapter.ts
+++ src/common/adapters/axios.adapter.ts
@@ -1,11 +1,13 @@
+import { Injectable } from '@nestjs/common';
 import axios, { type AxiosRequestConfig, type AxiosInstance } from 'axios';
+
 import { type HttpAdapter } from '../interfaces/http-adapter.interface';
 
 
+@Injectable()
 export class AxiosAdapter implements HttpAdapter {
 
   private axios:AxiosInstance = axios;
-
 
   async get<T>( url:string, config:AxiosRequestConfig ):Promise<T> {
     try {
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import axios, { type AxiosInstance } from 'axios';
 
 import { type PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { type HttpAdapter } from 'src/common/interfaces/http-adapter.interface';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios:AxiosInstance = axios;
 
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel:Model<Pokemon>
+    private readonly pokemonModel:Model<Pokemon>,
+    private readonly http:HttpAdapter
   ) {}
 
   async execute() {
 
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
+    const data = await this.http.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
 
     // const insertPromisesArray: Promise<Pokemon>[] = [];
 
